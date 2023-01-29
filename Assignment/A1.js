2e0f2c029c49c7aaad37eb709b6cd265e0e3ef42
manoj1 <manoj@selfmade.ninja>
2023-01-29 13:11:06 +0530

    init

---
+++ Assignment/A1.js
@@ -0,0 +1,14 @@
+let country, continent, population;
+country = "INDIA";
+continent = "Asia";
+population = 1400;
+const language = 'Tamil';
+
+// console.log(country,continent,population);
+// console.log(language);
+
+var southINDIA,northINDIA;
+northINDIA = population / 2
+southINDIA = northINDIA
+
+console.log(northINDIA,southINDIA)
